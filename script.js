2f6d10dcb63c818a28c68522665279088c77c840
thiripwint645 <thiripwint21@gmail.com>
2022-08-13 15:02:45 +0630

    second commit

--- script.js
+++ script.js
@@ -2,8 +2,18 @@ const newQuote = document.getElementById("new-quote");
 const quoteHtml = document.getElementById("quote");
 const author = document.getElementById("author");
 const quoteContainer = document.getElementById("quote-container");
+const loader = document.getElementById("loader");
 let quoteData = []; //array ချင်းလဲ let နဲ့ပဲရ
+const loading = () => {
+  loader.hidden = false;
+  quoteContainer.hidden = true;
+};
+const dataShow = () => {
+  loader.hidden = true;
+  quoteContainer.hidden = false;
+};
 const randomQuote = () => {
+  loading();
   const quote = quoteData[Math.floor(Math.random() * quoteData.length)];
   quoteHtml.textContent = quote.text; //json က လာမှာ
 
@@ -18,9 +28,11 @@ const randomQuote = () => {
     quoteHtml.classList.remove("long-quote");
     ("long-quote");
   }
+  dataShow();
 };
 newQuote.addEventListener("click", randomQuote);
 async function quoteGenerator() {
+  loading();
   const apiUrl = "https://type.fit/api/quotes";
   try {
     const response = await fetch(apiUrl);
